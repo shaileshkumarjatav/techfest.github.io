2a53b65dfbb16bbca8b70bf1b26b483e00a6105e
shaileshkumarjatav <shaileshkumarjatav211@gmail.com>
2017-12-08 20:38:22 +0530

    date

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@
 	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2017/12/15';
+	var finalDate = '2017/12/16';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
